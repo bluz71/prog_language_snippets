7ef6397856fb50737e92a0b4337cc446708ff140
Dennis B <bluz71@users.noreply.github.com>
2017-03-14 14:21:28 +1100

    More Elixir snippets.

---
+++ elixir/spawn_link2.exs
@@ -0,0 +1,22 @@
+defmodule SpawnLink2 do
+  import :timer, only: [sleep: 1]
+
+  def sad_function do
+    sleep 500
+    exit(:boom)
+  end
+
+  def run do
+    # Process.flag(:trap_exit, true)
+    # spawn_link(SpawnLink2, :sad_function, [])
+    spawn_monitor(SpawnLink2, :sad_function, [])
+    receive do
+      msg ->
+        IO.puts "MESSAGE RECEIVED: #{inspect msg}"
+    after 1000 ->
+        IO.puts "Nothing happened as far as I am concerned"
+    end
+  end
+end
+
+SpawnLink2.run
